d66bb5bc599e7b896b10a9a128755f180e0062ca
erassynathingo <erassywap@outlook.com>
2016-03-07 08:42:59 -0800

    .

--- Dashboard/js/app.js
+++ Dashboard/js/app.js
@@ -2,63 +2,10 @@
  * Created by erassy on 3/6/2016.
  */
 
-angular
-    .module('Emergency-App', ['ngMaterial'])
-    .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-        $scope.toggleLeft = buildDelayedToggler('left');
-        $scope.toggleRight = buildToggler('right');
-        $scope.isOpenRight = function(){
-            return $mdSidenav('right').isOpen();
-        };
-
-        function debounce(func, wait, context) {
-            var timer;
-            return function debounced() {
-                var context = $scope,
-                    args = Array.prototype.slice.call(arguments);
-                $timeout.cancel(timer);
-                timer = $timeout(function() {
-                    timer = undefined;
-                    func.apply(context, args);
-                }, wait || 10);
-            };
-        }
-        /**
-         * Build handler to open/close a SideNav; when animation finishes
-         * report completion in console
-         */
-        function buildDelayedToggler(navID) {
-            return debounce(function() {
-                $mdSidenav(navID)
-                    .toggle()
-                    .then(function () {
-                        $log.debug("toggle " + navID + " is done");
-                    });
-            }, 200);
-        }
-        function buildToggler(navID) {
-            return function() {
-                $mdSidenav(navID)
-                    .toggle()
-                    .then(function () {
-                        $log.debug("toggle " + navID + " is done");
-                    });
-            }
-        }
-    })
-    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-        $scope.close = function () {
-            $mdSidenav('left').close()
-                .then(function () {
-                    $log.debug("close LEFT is done");
-                });
-        };
-    })
-    .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-        $scope.close = function () {
-            $mdSidenav('right').close()
-                .then(function () {
-                    $log.debug("close RIGHT is done");
-                });
-        };
-    });
+var app = angular.module('Emergency-App', ["ngMaterial", "ngAnimate"]);
+app.controller('listController', function($scope) {
+    $scope.names = ["Erassy", "Frieda", "Lukas", "Leonard", "Tuli", "Bertie"];
+});
+app.controller('cardCtrl', function($scope) {
+    $scope.Location = 'Windhoek';
+});
