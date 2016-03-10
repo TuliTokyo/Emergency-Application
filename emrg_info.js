5827bc082ae67b2902abcfaec7b3f67130bfb028
bertie <coetzeealbertus@gmail.com>
2016-03-10 17:44:16 +0200

    all

--- emrg_info.js
+++ emrg_info.js
@@ -1,5 +1,5 @@
 console.log("*******************************************************************")
-console.log("Starting Server")
+console.log("*                       Starting Server                           *")
 console.log("*******************************************************************")
 
 const PORT=5000;
@@ -8,6 +8,7 @@ var Firebase = require("firebase");
 var myFirebaseRef = new Firebase("https://torrid-fire-226.firebaseio.com");
 var express = require("express");
 var app = express();
+var math = require("mathjs");
 
   app.get("/addUsr", function(req, res){
     var fname = req.param("fname");
@@ -35,6 +36,7 @@ app.get("/addService", function(req, res){
   var lat = req.param("lat");
   var long = req.param("long");
   var usrRef = myFirebaseRef.child("services").child(sname);
+  var comLoc = math.sum(lat, long);
 
   usrRef.set(
     {
@@ -46,9 +48,12 @@ app.get("/addService", function(req, res){
     "location":{
       "lat":lat,
       "long":long
-    }
+    },
+    "comLoc":comLoc
   });
-  console.log("added new Service...");
+  console.log('*************************');
+  console.log("*   added new Service   *");
+  console.log('*************************');
 });
 
 app.get("/emrg", function(req, res){
@@ -59,21 +64,87 @@ app.get("/emrg", function(req, res){
   var lname;
   var town;
   var phone;
-  var nameRef = myFirebaseRef.child("citizens").child(cell);
+  var userRef = myFirebaseRef.child("citizens").child(cell);
+  var usrLoc = math.sum(lat, long);
+  var largeLoc = null;
+  var smallLoc = null;
 
-  nameRef.once('value', function(snapshot) {
+  userRef.once('value', function(snapshot) {
   var data = snapshot.val();
   fname = data.fname;
   lname = data.lname;
   town = data.town;
   phone = cell;
-  console.log('name: ' + fname);
-  console.log('last name: ' + lname);
-  console.log('town: ' + town);
-  console.log('phone: ' + phone);
-  console.log('lat: ' + lat);
-  console.log('long: ' + long);
+  console.log('********************************************************************');
+  console.log('*       name:      ' + fname +'                                           *');
+  console.log('*       last name: ' + lname +'                                        *');
+  console.log('*       town:      ' + town +'                                       *');
+  console.log('*       phone:     ' + phone +'                                      *');
+  console.log('*       lat:       ' + lat +'                                      *');
+  console.log('*       long:      ' + long +'                                       *');
+  console.log('********************************************************************');
   });
+
+  var servicesRef = myFirebaseRef.child("services");
+
+    var smallerRef = servicesRef.orderByChild("comLoc").endAt(usrLoc).limitToLast(2);
+    smallerRef.on("value", function(snapshot) {
+      console.log('Snapshot Data: ' + snapshot.val());
+        if (snapshot.numChildren() == 1) {
+          snapshot.forEach(function(locSnapshot) {
+            var tempData = locSnapshot.val();
+            smallLoc = tempData.comLoc;
+            console.log("Closest location to the user (Smaller number): " + smallLoc);
+            console.log('smaller number is: ' + smallLoc);
+            return true;
+          });
+        } else {
+          console.log("No shorter number...");
+        }
+
+        var largerRef = servicesRef.orderByChild("comLoc").startAt(usrLoc).limitToFirst(2);
+        largerRef.on("value", function(snapshot) {
+          console.log('Snapshot Data: ' + snapshot.val());
+            if (snapshot.numChildren() == 1) {
+              snapshot.forEach(function(locSnapshot) {
+                var tempData = locSnapshot.val();
+                largeLoc = tempData.comLoc;
+                console.log("Closest location to the user (Larger number): " + largeLoc);
+                console.log('smaller number is: ' + largeLoc);
+                return true;
+              });
+            } else {
+              console.log("No Larger number...");
+            }
+
+            if (smallLoc == null || largeLoc == null) {
+              console.log('we have a null');
+              console.log('smallLoc: ' + smallLoc);
+              console.log('largeLoc: ' + largeLoc);
+
+            } else {
+              console.log('No Nulls');
+              var smallVar = math.var(usrLoc, smallLoc);
+              var largeVar = math.var(usrLoc, largeLoc);
+
+              if (smallVar < largeVar){
+                console.log('*************************************************');
+                console.log('*    The Smaller Location is the closest        *');
+                console.log('*************************************************');
+              }
+              if (smallVar > largeVar){
+                console.log('*************************************************');
+                console.log('*    The Larger Location is the closest         *');
+                console.log('*************************************************');
+              } else {
+                console.log('error for days....');
+              }
+
+            }
+        });
+    });
+
+
   });
 
 var server = app.listen(PORT, function(){
